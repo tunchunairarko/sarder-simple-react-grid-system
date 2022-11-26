6de7ace6a32eb87c18a642e3c7fc3ff35dd1966c
rakinsarder <rakin@marktomarket.io>
2022-11-26 10:49:06 +0000

    feat: changed file structure

---
+++ index.js
@@ -0,0 +1,5 @@
+import Row from "./components/Row";
+import Column from "./components/Column";
+import Container from "./components/Container";
+import "./assets/grid.scss";
+export { Row, Column, Container };
